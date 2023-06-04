e3a7e51e3cd7cea967d68d106ed94972f7fad250
shirelabergil <ghanaab@ac.sce.ac.il>
2023-06-04 22:42:22 +0300

    add tests

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // Connect to MongoDB
-const dbURI = 'mongodb+srv://ghanaab:1234567890@devops.4pnnlmu.mongodb.net/devops?retryWrites=true&w=majority';
+const dbURI =
+  'mongodb+srv://ghanaab:1234567890@devops.4pnnlmu.mongodb.net/devops?retryWrites=true&w=majority';
 mongoose
   .connect(dbURI, {
     useNewUrlParser: true,
@@ -26,10 +27,10 @@ mongoose
 
 // Define routes
 app.get('/', (req, res) => {
-  res.render('register');
+  res.render('add_student');
 });
 
-app.post('/register', async (req, res) => {
+app.post('/add_student', async (req, res) => {
   const { name, exam1, exam2, exam3 } = req.body;
   try {
     const student = new Student({
@@ -39,11 +40,15 @@ app.post('/register', async (req, res) => {
       exam3,
     });
     await student.save();
-    res.redirect('/');
+    res.sendStatus(302).redirect('/');
   } catch (err) {
     console.error('Error saving student', err);
-    res.redirect('/');
+    res.status(400).send('Invalid student data');
   }
 });
+
+
+
+
 
 module.exports = app;
--- tests/test1.test.js
+++ tests/test1.test.js
@@ -1,13 +1,57 @@
-const request = require('supertest')
-const app = require('../index')
+const request = require('supertest');
+const app = require('../index');
 
-describe("Test suite 1:" , ()=>{
-    // test("test 1:", async ()=>{
-    //     const res = await request(app).get('/')
-    //     expect(res.statusCode).toEqual(200)
-    // })
-    test("test 2:", async ()=>{
-        const res = await request(app).get('/1234')
-        expect(res.statusCode).toEqual(404)
-    })
+describe('Test the add_student route', () => {
+//  test('It should respond with a status code 302 for successful student creation', async () => {
+//   const response = await request(app)
+//     .post('/add_student')
+//     .send({
+//       name: 'John Doe',
+//       exam1: 90,
+//       exam2: 85,
+//       exam3: 95,
+//     });
+//   expect(response.statusCode).toBe(302);
+//   expect(response.header['location']).toBe('/');
+// });
+
+  test('It should respond with a status code 200 for the home route', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('It should respond with a status code 400 for invalid student data', async () => {
+    const response = await request(app)
+      .post('/add_student')
+      .send({
+        name: 'Invalid Student',
+        exam1: 'Not a number',
+        exam2: 85,
+        exam3: 95,
+      });
+    expect(response.statusCode).toBe(400);
+  });
+
+  test('It should respond with a status code 400 if name field is missing', async () => {
+    const response = await request(app)
+      .post('/add_student')
+      .send({
+        exam1: 90,
+        exam2: 85,
+        exam3: 95,
+      });
+    expect(response.statusCode).toBe(400);
+  });
+
+  test('It should respond with a status code 400 if exam1 field is missing', async () => {
+    const response = await request(app)
+      .post('/add_student')
+      .send({
+        name: 'John Doe',
+        exam2: 85,
+        exam3: 95,
+      });
+    expect(response.statusCode).toBe(400);
+  });
 });
+
