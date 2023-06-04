2c0ce6e15dcacd1659867f08cd3497452ddcf875
shirelabergil <ghanaab@ac.sce.ac.il>
2023-06-04 11:24:00 +0300

    connect to mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+//connect to mongoDB
+const dbURI = 'mongodb+srv://<ghanaab>:<1234567890>@devops.4pnnlmu.mongodb.net/devops?retryWrites=true&w=majority' ;
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true, 
+})
+    .then((result)=> app.listen(process.env.PORT || 3000) )
+    .catch((err)=> console.log(err) );
 
 app.get('/', (req,res)=>{
     res.send('Hello')
