7febba2974a1364f89b7b591b0d620506615d8d6
shirelabergil <ghanaab@ac.sce.ac.il>
2023-06-04 11:52:05 +0300

    fix syntax error

--- models/student.js
+++ models/student.js
@@ -3,9 +3,9 @@ const schema = mongoose.Schema;
 
 const studentSchema = new schema({
     name: String,
-    exam1 : Int,
-    exam2 : int,
-    exam3 : int
+    exam1 : Number,
+    exam2 : Number,
+    exam3 : Number
 }, {timestamps: true});
 
 const Student = mongoose.model('Student', studentSchema);
